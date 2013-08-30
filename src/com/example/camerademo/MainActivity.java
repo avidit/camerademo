07c81430e9bc10e832309683e6617dec4038b4ac
Raj Girimaji <rajgirimaji@Rajs-MacBook-Pro.local>
2013-08-29 23:57:45 -0400

    fixed errors

--- src/com/example/camerademo/MainActivity.java
+++ src/com/example/camerademo/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.camerademo;
 
 import java.io.File;
-import java.util.Date;
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -30,7 +31,8 @@ public class MainActivity extends Activity implements OnClickListener{
 	Bitmap photo;
 	Uri myfileUri = null;
 	private static final int CAMERA_REQUEST = 0; 
-
+	Bitmap bitmap = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -47,20 +49,32 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		Intent myint = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
 		myfileUri = Uri.fromFile(getPhoto());
+		myint.putExtra(MediaStore.EXTRA_OUTPUT, myfileUri);//RG - set this, so camera can capture the picture and store into this file
 		startActivityForResult(myint, CAMERA_REQUEST); 
 	}
 
 	private File getPhoto()
 	{
-		File directory = new File(
-				Environment.getExternalStoragePublicDirectory(
-						Environment.DIRECTORY_PICTURES), getPackageName());
+		//RG
+		File directory = Environment.getExternalStorageDirectory(); //you need 	this in the manifest: <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
 
+		directory=new File(directory.getAbsolutePath()+"/.temp/");
+
+		if(directory.exists() == false)
+		{
+			directory.mkdir();
+		}
 		Locale current = getResources().getConfiguration().locale;
 		SimpleDateFormat myformat = new SimpleDateFormat("yyMMdd_HHmmss", current);
 		String date = myformat.format(new Date());
-		return new File(directory.getPath() + File.separator + "IMG_" + date + ".jpg");
-		
+		try {
+			return File.createTempFile("IMG_" + date, ".jpg", directory);//RG - you need to CREATE an empty file, before you send it to the camera
+		} catch (IOException e) {
+			boolean exists = directory.exists();
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -69,16 +83,21 @@ public class MainActivity extends Activity implements OnClickListener{
 		{
 			if (resultCode == RESULT_OK)
 			{  
-				Uri photoUri  = data.getData();
-				String filestring = photoUri.getPath();
-
-				Bitmap photo = (Bitmap) data.getExtras().get("data"); 
-				preview.setImageBitmap(photo);
-
-				Bitmap bitmap = BitmapFactory.decodeFile(filestring);
-				BitmapDrawable drawable = new BitmapDrawable(this.getResources(),bitmap);
-				whole_photo.setScaleType(ImageView.ScaleType.FIT_CENTER);
-				whole_photo.setImageDrawable(drawable);
+				String filestring = myfileUri.getPath();
+				File dest = new File(filestring);
+				FileInputStream fis;
+				try {
+					fis = new FileInputStream(dest); //
+					if(bitmap != null)
+					{
+						bitmap.recycle();//RG - make sure you recycle to save memory
+					}
+					bitmap = BitmapFactory.decodeStream(fis);
+					preview.setImageBitmap(bitmap);
+					fis.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 			else if (resultCode == RESULT_CANCELED) 
 			{
